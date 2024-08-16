b03e89af6b4dbabbb1f4f9a06ee0873b42d8717f
IshanFTW <ishanjindal2003@gmail.com>
2024-08-16 15:01:22 +0530

    integrated weather api

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useEffect, useRef, useState } from 'react'
 import "./Weather.css"
 import search_icon from "../assets/search.png"
 import clear_icon from "../assets/clear.png"
@@ -8,29 +8,102 @@ import humidity_icon from "../assets/humidity.png"
 import rain_icon from "../assets/rain.png"
 import snow_icon from "../assets/snow.png"
 import wind_icon from "../assets/wind.png"
+import axios from 'axios'
+
+interface weatherDataProps {
+    humidity: number;
+    windSpeed: number;
+    temperature: number;
+    location: string;
+    icon: string,
+}
+
+type WeatherIcons = {
+    "01d": string;
+    "01n": string;
+    "02d": string;
+    "02n": string;
+    "03d": string;
+    "03n": string;
+    "04d": string;
+    "04n": string;
+    "09d": string;
+    "09n": string;
+    "10d": string;
+    "10n": string;
+    "13d": string;
+    "13n": string;
+};
 
 const Weather = () => {
+    const inputRef = useRef<HTMLInputElement>(null);
+    const [weatherData, setWeatherData] = useState<weatherDataProps | null> (null);
+
+    const allIcons = {
+        "01d": clear_icon,
+        "01n": clear_icon,
+        "02d": cloud_icon,
+        "02n": cloud_icon,
+        "03d": cloud_icon,
+        "03n": cloud_icon,
+        "04d": drizzle_icon,
+        "04n": drizzle_icon,
+        "09d": rain_icon,
+        "09n": rain_icon,
+        "10d": rain_icon,
+        "10n": rain_icon,
+        "13d": snow_icon,
+        "13n": snow_icon,
+    }
+    
+    async function fetchData(city: string) {
+        if (!city){
+            alert("Enter city name");
+            return;
+        };
+        try {
+            const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`);
+            const iconkey = response.data.weather[0].icon as keyof WeatherIcons;
+            const icon = allIcons[iconkey] || clear_icon;
+            console.log(response);
+            setWeatherData({
+                humidity: response.data.main.humidity,
+                windSpeed: response.data.wind.speed,
+                temperature: Math.floor(response.data.main.temp),
+                location: response.data.name,
+                icon: icon,
+            });
+        } catch (error) {
+            console.log("Error fetching weather data", error);
+            alert("Please enter correct city name")
+        }
+    }
+
+    useEffect(() => {
+        fetchData("London");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[])
   return (
     <div className='weather'>
         <div className='search-bar'>
-            <input type="text" placeholder='Search' />
-            <img src={search_icon} alt="" />
+            <input ref={inputRef} type="text" placeholder='Search'/>
+            <img src={search_icon} alt="" onClick={() => fetchData(inputRef.current?.value || "")}/>
         </div>
-        <img src={clear_icon} alt='' className='weather-icon'></img>
-        <p className='temperature'>16°C</p>
-        <p className='location'>London</p>
+        <img src={weatherData?.icon} alt='' className='weather-icon'></img>
+        <p className='temperature'>{weatherData?.temperature}°C</p>
+        <p className='location'>{weatherData?.location}</p>
         <div className="weather-data">
             <div className="col">
                 <img src={humidity_icon} alt="" />
                 <div>
-                    <p>91%</p>
+                    <p>{weatherData?.humidity}%</p>
                     <span>Humidity</span>
                 </div>
             </div>
             <div className="col">
                 <img src={wind_icon} alt="" />
                 <div>
-                    <p>3.6 m/s</p>
+                    <p>{weatherData?.windSpeed} km/h</p>
                     <span>Wind Speed</span>
                 </div>
             </div>
